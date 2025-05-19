3065cf9bf091dfc335abd0b7b323f5da2917c640
higor-silvadev <151585174+higor-silvadev@users.noreply.github.com>
2025-05-19 08:10:05 -0300

    feat: add new features and model

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -6,4 +6,29 @@ public partial class ContratacaoHospedagem : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+            Navigation.PushAsync(new HospedagemContratada());
+
+		}catch(Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+	}
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new Sobre());
+
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
